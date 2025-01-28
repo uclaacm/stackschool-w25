9e67e0a6166c4f5853c62e6f5b9bce68f3121b88
SPerrott22 <56176775+SPerrott22@users.noreply.github.com>
2025-01-28 15:38:30 -0800

    Added backbone from session 1

---
+++ session-1-intro-to-swift/BruinEatsApp/BruinEatsApp/BruinEatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BruinEatsAppApp.swift
+//  BruinEatsApp
+//
+//  Created by Samuel Perrott on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct BruinEatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
