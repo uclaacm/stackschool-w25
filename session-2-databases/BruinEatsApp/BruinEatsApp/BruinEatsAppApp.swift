e89a39d8489fab54df4439847d2053ed1d036905
Shiyu Ye <105144733+Monicaaawa@users.noreply.github.com>
2025-02-04 12:35:04 -0800

    upload session 2 demo

---
+++ session-2-databases/BruinEatsApp/BruinEatsApp/BruinEatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BruinEatsAppApp.swift
+//  BruinEatsApp
+//
+//  Created by Shiyu Ye on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct BruinEatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
